e5756d50fae853dfed2f11416561a71792e56939
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-01-30 19:29:18 +0700

    Set prefix pch file for tests

---
+++ BIS Homework AppTests/NetworkService/BISNetworkService_LoginSpec.m
@@ -0,0 +1,13 @@
+//
+//  BISNetworkService_LoginSpec.m
+//  BIS Homework App
+//
+//  Created by Ricardo Pramana Suranta on 1/30/15.
+//  Copyright (c) 2015 Ice House-Internship. All rights reserved.
+//
+
+#import "BISNetworkService_LoginSpec.h"
+
+@implementation BISNetworkService_LoginSpec
+
+@end
