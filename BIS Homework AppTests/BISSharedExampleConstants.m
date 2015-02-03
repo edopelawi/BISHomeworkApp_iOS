7d1e60c9ed17b9bc45ac5689d68edd39b0914363
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-02-03 15:10:12 +0700

    Add BISSharedExampleConstants class for test

---
+++ BIS Homework AppTests/BISSharedExampleConstants.m
@@ -0,0 +1,12 @@
+//
+//  BISSharedExampleConstants.m
+//  BIS Homework App
+//
+//  Created by Ricardo Pramana Suranta on 2/3/15.
+//  Copyright (c) 2015 Ice House-Internship. All rights reserved.
+//
+
+#import "BISSharedExampleConstants.h"
+
+NSString * const BISSharedExampleClassKey = @"BISSharedExampleClassKey";
+NSString * const BISSharedExampleNetworkServiceDependant = @"BISSharedExampleNetworkServiceDependant";
